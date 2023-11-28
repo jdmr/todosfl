77034a2b210ac2ce46360296f32e7ac19a0cd38d
J. David Mendoza <jdmendoza@swau.edu>
2023-11-28 12:12:53 -0600

    adds and removes todo list

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Todo App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Todo App'),
     );
   }
 }
@@ -55,71 +55,135 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _todoController = TextEditingController();
+  final List<String> _todos = [];
+  late FocusNode _todoFocusNode;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _todoFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _todoFocusNode.dispose();
+    super.dispose();
+  }
+
+  void addTodo(val) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _todos.add(val);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                      focusNode: _todoFocusNode,
+                      controller: _todoController,
+                      decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          hintText: 'Enter your todo',
+                          suffixIcon: IconButton(
+                              icon: const Icon(Icons.add),
+                              onPressed: () {
+                                if (!_formKey.currentState!.validate()) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content:
+                                            Text('Please enter your todo')),
+                                  );
+                                  _todoFocusNode.requestFocus();
+                                  return;
+                                }
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                      content: Text(
+                                          'Adding: ${_todoController.text}')),
+                                );
+                                addTodo(
+                                  _todoController.text,
+                                );
+                                _todoController.clear();
+                                _todoFocusNode.requestFocus();
+                              })),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your todo';
+                        }
+                        return null;
+                      },
+                      onFieldSubmitted: (value) {
+                        if (!_formKey.currentState!.validate()) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text('Please enter your todo')),
+                          );
+                          _todoFocusNode.requestFocus();
+                          return;
+                        }
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Adding: $value')),
+                        );
+                        addTodo(
+                          value,
+                        );
+                        _todoController.clear();
+                        _todoFocusNode.requestFocus();
+                      }),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const SizedBox(
+              height: 20,
             ),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: _todos.length,
+              itemBuilder: (context, index) {
+                final item = _todos[index];
+                return Dismissible(
+                  key: Key(item),
+                  onDismissed: (direction) {
+                    setState(() {
+                      _todos.removeAt(index);
+                    });
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('$item dismissed')),
+                    );
+                  },
+                  background: Container(
+                    color: Theme.of(context).colorScheme.error,
+                    child: const Icon(Icons.delete),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      title: Text(_todos[index]),
+                      tileColor: Theme.of(context).colorScheme.surfaceVariant,
+                    ),
+                  ),
+                );
+              },
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
